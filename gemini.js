e833d72fda4b584d2f1d8dbb3de6ed35d9e875aa
Siddhant Dhanaji Kadam <114551539+Siddh4194@users.noreply.github.com>
2023-12-25 18:44:57 +0530

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,5 +1,4 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const { array } = require("./verified-wikipedia-dev");
 const env = require('dotenv').config();
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
